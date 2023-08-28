3232179a6f71ebf94e070ababdf97e7a9ea668d7
chrisIyer <hellohotbox420@gmail.com>
2023-08-28 19:05:33 -0400

    saving for tonight.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { CreateRoot } from 'react-dom/client';
+
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Details from './Details'
